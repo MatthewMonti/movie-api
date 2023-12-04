d6024fc573a3f6f2a0a5896be41ecfd9293c4396
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-12-04 15:12:15 -0500

    backend validation

--- express/server.js
+++ express/server.js
@@ -168,11 +168,31 @@ app.get('/users/:Username', passport.authenticate ('jwt',
 
 //Add a user - WQRKS
 app.post('/users', async (req, res) => {
-  let hashedPassword = Users.hashPassword(req.body.Password);
+  // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty()
+  //which means "opposite of isEmpty" in plain english "is not empty"
+  //or use .isLength({min: 5}) which means
+  //minimum value of 5 characters are only allowed
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+    check('Featured', 'Feature in Movie Theater statement true or false not NULL').isBoolean(),
+], async(req, res) => {
+//check the validation object for errors
+  let errors = validationResult(req);
+}
+if (!error.isEmpty()) {
+  return res.status(422).json({errors: errors.array});
+}
+
+let hashedPassword = Users.hashPassword(req.body.Password);
   
-  await Users.findOne({ Username: req.body.Username })
+  await Users.findOne({ Username: req.body.Username }) //Search to see if a user with the requested username alreadyy exists
     .then((user) => {
       if (user) {
+        //If the user is found, send a response that it already exists
         return res.status(400).send(req.body.Username + ' already exists');
       } else {
         Users
@@ -204,6 +224,26 @@ app.post('/users', async (req, res) => {
 //BIRTHDAY 
 //FAVORITE MOVIE
 app.put('/users/:Username',  passport.authenticate('jwt', { session: false }), async (req, res) => {
+    // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty()
+  //which means "opposite of isEmpty" in plain english "is not empty"
+  //or use .isLength({min: 5}) which means
+  //minimum value of 5 characters are only allowed
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+    check('Featured', 'Feature in Movie Theater statement true or false not NULL').isBoolean(),
+], async(req, res) => {
+//check the validation object for errors
+  let errors = validationResult(req);
+}
+if (!error.isEmpty()) {
+  return res.status(422).json({errors: errors.array});
+}
+
+let hashedPassword = Users.hashPassword(req.body.Password);
   await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
       Username: req.body.Username,
