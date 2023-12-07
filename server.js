a4e7ed982455b7bf0cae26548f1417d4b5ef3f9c
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-12-07 13:31:42 -0500

    address mongoose updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect('mongodb://https://mattmyflixdb-5a9ddca1adfe.herokuapp.com/')
+mongoose.connect('mongodb://https://matt-myflixdb-16b9ec7c23cd.herokuapp.com/')
 .then(() => console.log('Mongoose Connected'))
 .catch((err) => {console.error(err); });
 const express = require('express'),
