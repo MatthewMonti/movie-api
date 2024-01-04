a7b8c7d94c758ae2b0a6c274bbfd99fde7c2020e
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-04 15:30:49 -0500

    allow orgin wrong URL

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(morgan('common'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 let cors = require('cors');
-let allowedOrigins = ['https://mattmoviedatabase-221aec62abb6.herokuapp.com'];
+let allowedOrigins = ['https://movies-flex-6e317721b427.herokuapp.com'];
 const { check, validationResult } = require('express-validator');
 
 app.use(cors({
