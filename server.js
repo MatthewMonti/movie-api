7a4be25a8fdc0d151ed2e8814782045d353c6a17
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-05 17:24:44 -0500

    allowed origins updated

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(morgan('common'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 let cors = require('cors');
-let allowedOrigins = ['https:www.localhost:8080','https://movies-flex-6e317721b427.herokuapp.com'];
+let allowedOrigins = ['https://www.localhost:8080','https://movies-flex-6e317721b427.herokuapp.com'];
 const { check, validationResult } = require('express-validator');
 
 app.use(cors({
