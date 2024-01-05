def73dda947cfb1cba97e958a6f216778f8cc340
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-05 18:36:28 -0500

    ENV IS CRAP

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect(process.env.CONNECTION_URI);
+mongoose.connect('mongodb+srv://Matthew:Railroad@cluster0.vz9ijr2.mongodb.net/myFlixDB?retryWrites=true&w=majority');
 const express = require('express'),
 bodyParser = require('body-parser'),
 morgan = require('morgan');
