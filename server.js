52ba3b9f37a11b6ed46d63c5095857da495396f5
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-07 16:46:36 -0500

    update endpoint open source documentation.html

--- server.js
+++ server.js
@@ -138,8 +138,7 @@ app.get("/movies/director/:name", passport.authenticate ('jwt',
 
 
 //API DOCUMENTATION WORKS
-app.get('/movies/about_api/documentation', passport.authenticate ('jwt',
- {session: false}), async (req, res) => {             
+app.get('/movies/about_api/documentation', async (req, res) => {             
   res.status(200).sendFile('./documentation.html', { root: __dirname });
 })
 
