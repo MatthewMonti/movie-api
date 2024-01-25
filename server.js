ffb187f8b81f66f09030fa65a5a2afa0ed647964
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-25 14:51:27 -0500

    internet not local pc

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(morgan('common'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 let cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'https://movies-flex-6e317721b427.herokuapp.com'];
+let allowedOrigins = [ 'https://movies-flex-6e317721b427.herokuapp.com'];
 const { check, validationResult } = require('express-validator');
 
 app.use(cors({
@@ -44,7 +44,7 @@ app.get('/', async (req, res) => {
 });
 
 //WELCOME MESSAGE WORKS
-app.get('/about_api', async (req, res) => {
+app.get('/api/about_api', async (req, res) => {
   res.status(200).sendFile('./public/doc.html', { root: __dirname });
   req.responseText += '<small>Requested at: ' + 
   req.requestTime + '</small>';
@@ -247,7 +247,7 @@ app.post('api/users',
 // USER NAME
 //EMAIL
 //BIRTHDAY 
-app.put('api/users/:id', passport.authenticate ('jwt',
+app.put('api/users/', passport.authenticate ('jwt',
 {session: false}), 
   // Validation logic here for request
   //you can either use a chain of methods like .not().isEmpty()
