ec74ee0fa1974f74cade08ea4fff59c0ff789a6a
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-27 13:11:58 -0500

    fix error 3

--- server.js
+++ server.js
@@ -282,10 +282,10 @@ app.post('/api/:account/Favorite/:film_id', passport.authenticate('jwt',
    },
    { new: true }) // This line makes sure that the updated document is returned
   .then((updatedUser) => {
-    if (updatedUser === 1) {
-      res.status(200).send(req.params.Favorite + ' favorite film adding.');
-    } else {
+    if (!updatedUser === 1) {
       res.status(400).send(req.params.Favorite + ' already in account.');
+    } else {
+      res.status(200).send(req.params.Favorite + ' favorite film adding.');
     }
   })
   .catch((err) => {
@@ -302,7 +302,7 @@ app.delete('/api/:account/Favorite/:film_id', passport.authenticate('jwt',
    },
    { new: true }) // This line makes sure that the updated document is returned
   .then((updatedUser) => {
-    if (updatedUser === 1) {
+    if (!updatedUser === 1) {
       res.status(400).send(req.params.Favorite + ' no film match to delete.');
     } else {
       res.status(200).send(req.params.Favorite + ' favorite film deleted.');
