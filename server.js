897641ba3a4d33531900f3ba385eb33ce8b3ee71
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-27 12:37:41 -0500

    logic only one id

--- server.js
+++ server.js
@@ -282,10 +282,10 @@ app.post('/api/:account/Favorite/:film_id', passport.authenticate('jwt',
    },
    { new: true }) // This line makes sure that the updated document is returned
   .then((updatedUser) => {
-    if (updatedUser <= 1) {
-      res.status(200).send(req.body.Favorite + ' favorite film adding.');
-    } else {
+    if (!updatedUser === 1) {
       res.status(400).send(req.body.Favorite + ' already in account.');
+    } else {
+      res.status(200).send(req.body.Favorite + ' favorite film adding.');
     }
   })
   .catch((err) => {
@@ -302,10 +302,10 @@ app.delete('/api/:account/Favorite/:film_id', passport.authenticate('jwt',
    },
    { new: true }) // This line makes sure that the updated document is returned
   .then((updatedUser) => {
-    if (updatedUser <= 1) {
-      res.status(200).send(req.body.Favorite + ' favorite film deleted.');
-    } else {
+    if (!updatedUser === 1) {
       res.status(400).send(req.body.Favorite + ' no film match to delete.');
+    } else {
+      res.status(200).send(req.body.Favorite + ' favorite film deleted.');
     }
   })
   .catch((err) => {
