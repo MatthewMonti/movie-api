81df848f327b7123f58872024f1f49314c69dd92
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-27 12:06:27 -0500

    update

--- server.js
+++ server.js
@@ -282,7 +282,7 @@ app.post('/api/:account/Favorite/:film_id', passport.authenticate('jwt',
    },
    { new: true }) // This line makes sure that the updated document is returned
   .then((updatedUser) => {
-    if (!updatedUser == 0) {
+    if (updatedUser == 0) {
       res.status(400).send(req.body.Favorite + ' already in account.');
     } else {
       res.status(200).send(req.body.Favorite + ' favorite film added.');
@@ -302,7 +302,7 @@ app.delete('/api/:account/Favorite/:film_id', passport.authenticate('jwt',
    },
    { new: true }) // This line makes sure that the updated document is returned
   .then((updatedUser) => {
-    if (!updatedUser == 0) {
+    if (updatedUser == 0) {
       res.status(400).send(req.body.Favorite + ' no film match to delete.');
     } else {
       res.status(200).send(req.body.Favorite + ' favorite film deleted.');
