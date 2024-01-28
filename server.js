278221951797ab5c761d59ca66105abf1c2404af
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-01-28 02:22:26 -0500

    reset

--- server.js
+++ server.js
@@ -37,21 +37,21 @@ require('./passport.js');
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags:'a'})
 app.use(morgan('combined', {stream:accessLogStream}));
 
-//WELCOME MESSAGE - WORKS
+//WELCOME MESSAGE 
 app.get('/', async (req, res) => {
   res.status(200).send("Welcome to myFlix Movie Database"),
   req.responseText += '<small>Requested at: ' + 
   req.requestTime + '</small>';
 });
 
-//WELCOME MESSAGE - WORKS
+//WELCOME MESSAGE 
 app.get('/api/about', async (req, res) => {
   res.status(200).sendFile('./public/doc.html', { root: __dirname });
   req.responseText += '<small>Requested at: ' + 
   req.requestTime + '</small>';
 });
 
-//MOVIES LIST - WORKS
+//MOVIES LIST 
 app.get('/api/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
@@ -63,13 +63,13 @@ app.get('/api/movies', passport.authenticate('jwt', { session: false }), async (
     });
 });
 
-//TITLE SEARCH = 
+//TITLE SEARCH 
 app.get('/api/movies/title/:label', passport.authenticate('jwt', 
 { session: false }), async (req, res) => {
   await Movies.find({ Title: req.params.label })
   .then((label) => {
     if (label.length == 0) {
-      res.status(400).send(req.params.label + ' movie title not in database and remember 1st letter most be capital.');
+      res.status(400).send(req.params.label + ' movie title not in database.');
     } else {
       res.status(200).json(label)
     }
@@ -80,12 +80,12 @@ app.get('/api/movies/title/:label', passport.authenticate('jwt',
   });
 });
 
-//RELEASE YEAR = WORKS
+//RELEASE YEAR 
 app.get('/api/movies/release/:year', async (req, res) => {
   await Movies.find({ Release: req.params.year})
   .then((year) => {
     if (year.length == 0) {
-      res.status(400).send(req.params.year + " film production year filter movies that show we don't any films match criteria please check date");
+      res.status(400).send(req.params.year + " film year does not have any movies yet or invalid year");
     } else {
       res.status(200).json(year)
     }
@@ -97,12 +97,12 @@ app.get('/api/movies/release/:year', async (req, res) => {
 });
 
 
-//RATED FOR FIND APPROPRIATE AUDIENCE = WORKS 
+//RATED FOR FIND APPROPRIATE AUDIENCE WORKS 
 app.get('/api/movies/rated/:audience', async (req, res) => {
   await Movies.find({ Rated: req.params.audience })
   .then((audience) => {
     if (audience.length == 0) {
-      res.status(400).send(req.params.audience + ' demographic is either not serviced by our database or is mistype.');
+      res.status(400).send(req.params.audience + ' demographic is either not serviced by database or is invalid entry.');
     } else {
       res.status(200).json(audience)
     }
@@ -113,12 +113,12 @@ app.get('/api/movies/rated/:audience', async (req, res) => {
   });
 });
 
-//Quality of FILMS 
+//Quality of FILMS WORKS
 app.get('/api/movies/rating/:percentage', async (req, res) => {
   await Movies.find({ Rating: req.params.percentage })
   .then((percentage) => {
-    if (percentage.length == 0) {
-      res.status(400).send(req.params.percentage + ' rotten tomatoes percentage criteria could not match a film or is a mistype percentage (0-100)');
+    if (!percentage) {
+      res.status(400).send(req.params.percentage + ' rotten tomatoes percentage is either a rating that is yet to match a film in our database or invalid percentage ourside the range of (0-100)');
     } else {
       res.status(200).json(percentage)
     }
@@ -129,12 +129,12 @@ app.get('/api/movies/rating/:percentage', async (req, res) => {
   });
 });
  
-//GENRE SEARCH FOR MOVIE 
+//GENRE SEARCH FOR MOVIE
 app.get('/api/movies/genre/:genreName', async (req, res) => {
   await Movies.find({'Genre.Name': req.params.genreName})
   .then((movies) => {
     if (movies.length == 0) {
-      res.status(400).send(req.params.genreName + ' category not in our database or forgot to capitalize 1st letter of genre name.');
+      res.status(400).send(req.params.genreName + ' category not in our database sorry we consider more additions in the future.');
     } else {
       res.status(200).json(movies)
     }
@@ -151,7 +151,7 @@ app.get("/api/movies/director/:name", async (req, res) => {
   Movies.find({'Director.Name': req.params.name })
   .then((movies) => {
     if (movies.length == 0) {
-      res.status(400).send(req.params.name + ' is a director not yet added to database please search someone else.');
+      res.status(400).send(req.params.name + ' is a director not yet added to database please try someone else.');
     } else {
       res.status(200).json(movies)
     }
@@ -163,17 +163,17 @@ app.get("/api/movies/director/:name", async (req, res) => {
 });
 
 //Add a user - WORKS error works
-app.post('/api/user',
+app.post('/api/users',
   // Validation logic here for request
   //you can either use a chain of methods like .not().isEmpty()
   //which means "opposite of isEmpty" in plain english "is not empty"
   //or use .isLength({min: 5}) which means
   //minimum value of 5 characters are only allowed
   [
-    check('Username', 'Username is required at least 5 letters').isLength({min: 5}).isunique(),
-    check('Username', 'Username contains non alphanumeric characters - not allowed.').isunique().isAlphanumeric(),
-    check('Password', 'Password is required').isunique().not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isunique().isEmail()
+    check('Username', 'Username is required at least 5 letters').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
   ], async (req, res) => {
 
   // check the validation object for errors
@@ -214,11 +214,11 @@ app.post('/api/user',
 
 
 
-/// USER CAN UPDATE FOLLOWING - NOT
+/// USER CAN UPDATE FOLLOWING - WORKS
 // USER NAME
 //EMAIL
 //BIRTHDAY 
-app.put('api/user/:identity', passport.authenticate ('jwt',
+app.put('api/users/:identity', passport.authenticate ('jwt',
 {session: false}), 
   // Validation logic here for request
   //you can either use a chain of methods like .not().isEmpty()
@@ -226,10 +226,10 @@ app.put('api/user/:identity', passport.authenticate ('jwt',
   //or use .isLength({min: 5}) which means
   //minimum value of 5 characters are only allowed
   [
-    check('Username', 'Username is required').isunique().isLength({min: 5}),
-    check('Username', 'Username contains non alphanumeric characters - not allowed.').isunique({max:1}).isAlphanumeric(),
-    check('Password', 'Password is required').isunique().not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isunique().isEmail()
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
   ], passport.authenticate('jwt', {session: false}), async (req, res) => {
 
   // check the validation object for errors
@@ -256,9 +256,9 @@ app.put('api/user/:identity', passport.authenticate ('jwt',
   .then((updatedUser) => {
     res.json(updatedUser);
     if (!updatedUser) {
-      res.status(400).send(req.param.identity + ' user has nothing to update.');
+      res.status(400).send(req.param._id + ' user has nothing to update.');
     } else {
-      res.status(200).send(req.param.identity+ ' user information has been updated.');
+      res.status(200).send(req.param._id + ' user information has been updated.');
     }
   })
   .catch((error) => {
@@ -267,15 +267,15 @@ app.put('api/user/:identity', passport.authenticate ('jwt',
   });
 });
 
-// Delete a user by User Account = WORKS
-app.delete('/api/user/:identity', passport.authenticate('jwt', 
+// Delete a user by username
+app.delete('/api/user/:identity/film_id', passport.authenticate('jwt', 
 { session: false }), async (req, res) => {
   await Users.findByIdAndDelete({_id: req.params.identity})
     .then((identity) => {
       if (identity.length == 0) {
-        res.status(400).send(req.params.identity + ' user was not in our records. ');
+        res.status(400).send(req.params._id + ' user was not in our records. ');
       } else {
-        res.status(200).send(req.params.identity + ' user was removed from our records.');
+        res.status(200).send(req.params._id + ' user was removed from our records.');
       }
     })
     .catch((err) => {
@@ -285,7 +285,7 @@ app.delete('/api/user/:identity', passport.authenticate('jwt',
 });
 
 // Add a movie to a user's list of favorites
-app.post('/api/user/favorite/:identity/:film_id', passport.authenticate('jwt', 
+app.post('/api/user/:identity/:film_id', passport.authenticate('jwt', 
 { session: false }), async (req, res) => {
   await Users.findByIdAndUpdate({ _id: req.params.identity }, {
      $addToSet: { Favorite: req.params.film_id}
@@ -305,7 +305,7 @@ app.post('/api/user/favorite/:identity/:film_id', passport.authenticate('jwt',
 });
 
 // Delete a movie to a user's list of favorites
-app.delete('/api/user/favorite/:identity/:film_id', passport.authenticate('jwt', 
+app.delete('/api/user/:identity/Favorite/:film_id', passport.authenticate('jwt', 
 { session: false }), async (req, res) => {
   await Users.findByIdAndDelete({_id: req.params.identity}, {
      $deleteToSet: {Favorite: req.params.film_id}
