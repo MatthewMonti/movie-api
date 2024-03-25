3b5c9d643feca2797d01c289662ead020d66d731
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-03-25 01:04:57 -0400

    youtube datatype and required more properties

--- models.js
+++ models.js
@@ -31,8 +31,9 @@ let movieSchema = mongoose.Schema({
     Birth: {type: Date, required:[true, 'Birth date of director required']},
     Death: {type: Date}
   },
-  Image: {type: String},
+  Image: {type: String, required:[true, 'Poster Image file is missing']},
   Featured: {type: String},
+  url: {type: String, required: [true, 'unable to get movie']}
 });
 
 let userSchema = mongoose.Schema({
