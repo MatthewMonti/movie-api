5868730f840be55c7bc33ec6ade921dde9ca14d8
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2024-05-17 14:54:04 -0400

    REQUIRE LIST of FAV USER FILMS

--- server.js
+++ server.js
@@ -273,7 +273,7 @@ app.post('/favorite', passport.authenticate('jwt',
 { session: false }), async (req, res) => {
   console.log("Tanking")
   await Users.findOneAndUpdate({ Username: req.body.Username }, {
-    $set: { Favorite: req.body.Favorite }
+    $addtoSet: { Favorite: req.body.Favorite }
    },
    { new: true }) // This line makes sure that the updated document is returned
   .then((updatedUser) => {
